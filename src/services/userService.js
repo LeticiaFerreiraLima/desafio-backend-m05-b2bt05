d23ae7799c6e57dcb04fe999abd2bb87fd7dd388
marianagsoares <marianasoares.ti@gmail.com>
2023-10-14 16:20:31 -0300

    feat: add update user method

--- src/services/userService.js
+++ src/services/userService.js
@@ -16,6 +16,28 @@ const createUser = async (name, email, password) => {
   return await userRepository.insertUser(name, email, encryptedPassword);
 };
 
+const updateUser = async (name, email, password, id) => {
+
+  if (!name || !email || !password)
+    throwCustomError("Preencha todos os campos obrigatórios.", 400);
+
+  const users = await userRepository.selectUserById(id);
+
+  if(users.length < 1)
+    throwCustomError("O usuário não foi encontrado", 404);
+  
+    const isEmailDifferent = email !== users[0].email;
+    const userFound = users.length > 0;
+
+  if( isEmailDifferent && userFound)
+    throwCustomError("O usuário não pode ser editado", 400);
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  return await userRepository.updateUser(name, email, encryptedPassword, id);
+};
+
 module.exports = {
   createUser,
+  updateUser
 };
