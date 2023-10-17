4b5a0bcf5a9035a4e99f911ca4354999f5dbdc71
Leticia Lima <leticiascferreira@gmail.com>
2023-10-16 22:59:08 -0300

    refactor: mudança nos dados de conexão com banco

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const knex = require('knex')({
 	client: 'pg',
 	connection: {
-		host: 'localhost',
+		host: process.env.DB_HOST,
 		user: process.env.DATABASE_USER,
 		password: process.env.DATABASE_PASSWORD,
 		database: process.env.DATABASE_NAME
